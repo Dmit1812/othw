b28b9ab8f42e16b38f77ec938bcea2e164a9113c
Dmit1812 <134962151+Dmit1812@users.noreply.github.com>
2023-07-07 11:10:27 +0300

    Fixes to comments and multitasking

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -20,90 +20,145 @@ type ListItem struct {
 
 type list struct {
 	len   int
+	mu    sync.Mutex
 	front *ListItem
 	back  *ListItem
 }
 
-var mutex sync.Mutex
-
+// Len returns the length of the list.
 func (l *list) Len() int {
-	mutex.Lock()
-	defer mutex.Unlock()
+	// critical section start, it ends when return is called
+	l.mu.Lock()
+	defer l.mu.Unlock()
 	return l.len
 }
 
+// Front returns address of the front ListItem of the list.
 func (l *list) Front() *ListItem {
-	mutex.Lock()
-	defer mutex.Unlock()
+	// critical section start, it ends when return is called
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
 	return l.front
 }
 
+// Back returns address of the back ListItem of the list.
 func (l *list) Back() *ListItem {
-	mutex.Lock()
-	defer mutex.Unlock()
+	// critical section start, it ends when return is called
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
 	return l.back
 }
 
+// pushFront private method adds an already created ListItem to the front of the list
+// BEWARE! it assumes that the mutex lock should be set by the caller
 func (l *list) pushFront(i *ListItem) *ListItem {
-	mutex.Lock()
-	defer mutex.Unlock()
+	// exit if i is nil
 	if i == nil {
 		return nil
 	}
+
+	// initialize the new front of the list
 	i.Prev = nil
 	i.Next = l.front
+
+	// if our front is initialized with pointer set old front to point to i (new front)
 	if l.front != nil {
 		l.front.Prev = i
 	} else {
+		// if our front is nil then it's the first element and both front and back should point to it
 		l.back = i
 	}
+
+	// set front to the i
 	l.front = i
+
+	// increase array size
 	l.len++
 	return i
 }
 
+// PushFront adds a new value to the back of the list.
 func (l *list) PushFront(v interface{}) *ListItem {
+	// critical section start, it ends when return is called
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
 	return l.pushFront(&ListItem{Value: v, Next: nil, Prev: nil})
 }
 
+// PushBack adds a new value to the back of the list.
 func (l *list) PushBack(v interface{}) *ListItem {
-	mutex.Lock()
-	defer mutex.Unlock()
+	// critical section start
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	// create a new ListItem with value from v
 	newback := &ListItem{Value: v, Next: nil, Prev: l.back}
+
+	// if out back is initilized then make sure that previous element points to new back element
 	if l.back != nil {
 		l.back.Next = newback
 	} else {
+		// if our back is nil then newback is the first element and both front and back should point to it
 		l.front = newback
 	}
+	// set back to the newback element
 	l.back = newback
+	// increase array size
 	l.len++
 	return newback
 }
 
-func (l *list) Remove(i *ListItem) {
-	mutex.Lock()
-	defer mutex.Unlock()
+// remove removes a ListItem from the list
+// no checking if the item is in the list performed
+// BEWARE! it assumes that the mutex lock should be set by the caller
+func (l *list) remove(i *ListItem) {
+	// exit if i is nil
 	if i == nil {
 		return
 	}
+
+	// if previous element of element i is defined, then make sure that it points to the element that is after alement i
 	if i.Prev != nil {
 		i.Prev.Next = i.Next
 	} else {
+		// there were no previous element, so we are removing the first element. Next element to current becomes new first
 		l.front = i.Next
 	}
+	// if next element of element i is defined, then make sure that it points back to the element that is before i
 	if i.Next != nil {
 		i.Next.Prev = i.Prev
 	} else {
+		// there was no next element, so we are removing the last element. Previous element to current becomes new last
 		l.back = i.Prev
 	}
+	// we removed an element from the list reduce array size
 	l.len--
 }
 
+// Remove will set mutex lock and call the remove to remove a ListItem from the list
+func (l *list) Remove(i *ListItem) {
+	// critical section start, it ends when return is called
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.remove(i)
+}
+
+// MoveToFront moves a ListItem to the front of the list
+// no checking if the item is from the the list performed
 func (l *list) MoveToFront(i *ListItem) {
-	l.Remove(i)
+	// critical section start, it ends when return is called
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.remove(i)
 	l.pushFront(i)
 }
 
+// NewList creates a new list
 func NewList() List {
 	return new(list)
 }
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -59,16 +59,15 @@ func TestList(t *testing.T) {
 		a := []int{40, 50, 60, 70, 80, 10, 1000, 15, 75, 30, 90, 1500}
 		threads := 20
 
+		// spawn `threads` goroutines where each would populate the list
 		for i := 0; i < threads; i++ {
 			wg.Add(1)
 			go func(index int, array []int) {
 				defer wg.Done()
 				for i, v := range array {
-					if i%2 == 0 {
-						l.PushFront(v)
-					} else {
-						l.PushBack(v)
-					}
+					_ = i
+					l.PushFront(v)
+					l.PushBack(v)
 				}
 				_ = index
 			}(i, a)
@@ -78,6 +77,7 @@ func TestList(t *testing.T) {
 
 		var incorrectPointer bool
 
+		// create a slice of values from list from front to back
 		elemsfb := make([]int, 0, l.Len())
 		for i := l.Front(); i != nil; i = i.Next {
 			elemsfb = append(elemsfb, i.Value.(int))
@@ -93,6 +93,7 @@ func TestList(t *testing.T) {
 			}
 		}
 
+		// create a slice of values from list from back to front
 		elemsbf := make([]int, 0, l.Len())
 		for i := l.Back(); i != nil; i = i.Prev {
 			elemsbf = append(elemsbf, i.Value.(int))
@@ -107,17 +108,45 @@ func TestList(t *testing.T) {
 				}
 			}
 		}
-		// reverse the list
+		// reverse the slice elemsbf so it can be compared with elemsfb
 		sort.SliceStable(elemsbf, func(i, j int) bool {
 			return i > j
 		})
 
-		require.Equal(t, len(elemsfb), l.Len())
-		require.Equal(t, len(elemsbf), l.Len())
-		require.Equal(t, threads*len(a), l.Len())
-		require.False(t, incorrectPointer)
-		require.Equal(t, elemsbf, elemsfb)
+		// it is expected that:
+		// 1 - count of of elements from front to back and from back to front should be equal to list length
+		// 2 - number of elements in list is equal to `threads` * length of initialization array
+		require.Truef(t, len(elemsfb) == l.Len() && len(elemsbf) == l.Len() && 2*threads*len(a) == l.Len(), "number of items in List if traversed front to back (%d) should be equal to list length stored inside (%d) and to back to front (%d) and to threads * length (%d)", len(elemsfb), l.Len(), len(elemsbf), threads*len(a))
+		// 3 - elements correctly reference each other and back
+		require.Falsef(t, incorrectPointer, "elements should correctly reference each other and back")
+		// 4 - values are same when we compare front to back and reversed back to front
+		require.Equalf(t, elemsbf, elemsfb, "values shall be the same when we compare list created by front to back traversal and reversed list done back to front")
+	})
 
+	// Verify that list is consistent after removing everything from it
+	t.Run("remove all", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(20)     // [20]
+		l.PushFront(10)    // [10, 20]
+		l.PushBack(30)     // [10, 20, 30]
+		l.Remove(l.Back()) // [10, 20]
+		l.Remove(l.Back()) // [10]
+		l.Remove(l.Back()) // []
+
+		require.Equal(t, 0, l.Len())
+		require.Nil(t, l.Front())
+		require.Nil(t, l.Back())
+	})
+
+	// Ensure first pushback works correctly
+	t.Run("first pushback", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(20)  // [10]
+		l.PushFront(10) // [10, 20]
+		l.PushBack(30)  // [10, 20, 30]
+		require.Equal(t, 3, l.Len())
 	})
 
 }
